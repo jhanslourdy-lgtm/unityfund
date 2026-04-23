cd2548b7cb20ecb6506d5674b3bba0e6ef1614cb
jhanslourdy-lgtm <jhanslourdy@gmail.com>
2026-04-23 13:44:52 -0400

    upload image 1

--- src/main/java/com/securityapp/gofundme/services/FileStorageService.java
+++ src/main/java/com/securityapp/gofundme/services/FileStorageService.java
@@ -1,14 +1,13 @@
 package com.securityapp.gofundme.services;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import org.springframework.util.ObjectUtils;
 
 @Service
 public class FileStorageService {
@@ -20,12 +19,13 @@ public class FileStorageService {
             @Value("${CLOUDINARY_API_KEY:}") String apiKey,
             @Value("${CLOUDINARY_API_SECRET:}") String apiSecret) {
         
-        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", cloudName,
-            "api_key", apiKey,
-            "api_secret", apiSecret,
-            "secure", true
-        ));
+        Map<String, Object> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        config.put("secure", true);
+        
+        this.cloudinary = new Cloudinary(config);
     }
 
     public String saveCampaignImage(MultipartFile file) throws IOException {
@@ -37,14 +37,12 @@ public class FileStorageService {
     }
 
     private String upload(MultipartFile file, String folder) throws IOException {
-        Map<?, ?> uploadResult = cloudinary.uploader().upload(
-            file.getBytes(),
-            ObjectUtils.asMap(
-                "folder", folder,
-                "resource_type", "image",
-                "overwrite", true
-            )
-        );
+        Map<String, Object> options = new HashMap<>();
+        options.put("folder", folder);
+        options.put("resource_type", "image");
+        options.put("overwrite", true);
+        
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
         return (String) uploadResult.get("secure_url");
     }
 }
